d954cb79dfe853161b53cffdc9bae422967c0d2f
SebasCodex <sebasdefinitivo1@gmail.com>
2026-02-28 12:22:13 -0500

    primer comit

---
+++ heroe1.js
@@ -0,0 +1,13 @@
+let nombre = "ironman";
+let poder = 1000;
+const velocidad = 150;
+const villano = "thanos";
+const boleano = true;
+
+
+console.log("el nombre de mi heroe es:" + nombre)
+console.log("su poder es de más de:" + poder)
+console.log("su velocidad es una constante y es:" + velocidad)
+console.log("su villano siempre sera:" + villano)
+console.log("es un heroe:" + boleano)
+

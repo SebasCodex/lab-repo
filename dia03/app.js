78aa18fdbad64aab09b56201a6a5da5ad437ac30
SebasCodex <sebasdefinitivo1@gmail.com>
2026-03-03 11:42:05 -0500

    se agregaron dos platos nuevos a menu

--- dia03/app.js
+++ dia03/app.js
@@ -2,7 +2,10 @@
 let menu = [
     { nombre: "Arroz con pollo", precio: 12, stock: 5 },
     { nombre: "Lomo saltado", precio: 18, stock: 3 },
-    { nombre: "Sopa", precio: 8, stock: 10 }
+    { nombre: "Sopa", precio: 8, stock: 10 },
+    { nombre: "ARROZ CHAUFA", precio: 16, stock: 9 },
+    { nombre: "CAUSA", precio: 20, stock: 15 }
+
 ];
 
 // 2) FUNCIÓN: renderizar (mostrar) el menú en pantalla
